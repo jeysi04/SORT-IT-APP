97d3f1b0a966e434dcb35805638466316f089246
Jaycee Cadag <cadagjaycee37@gmail.com>
2026-03-13 01:06:52 +0800

    Redesigning the app menu.

--- app/src/main/java/com/example/sort_it_json/MainActivity.kt
+++ app/src/main/java/com/example/sort_it_json/MainActivity.kt
@@ -79,32 +79,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         aboutBtn.setOnClickListener {
-            resetNavColors()
-            ImageViewCompat.setImageTintList(aboutBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
             replaceFragment(AboutFragment())
         }
 
         recycleBtn.setOnClickListener {
-            resetNavColors()
-            ImageViewCompat.setImageTintList(recycleBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
             replaceFragment(SampleDecideFragment())
         }
 
         faqBtn.setOnClickListener {
-            resetNavColors()
-            ImageViewCompat.setImageTintList(faqBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
             replaceFragment(FaqFragment())
         }
 
         feedbackBtn.setOnClickListener {
-            resetNavColors()
-            ImageViewCompat.setImageTintList(feedbackBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
             replaceFragment(feedbackFragment())
         }
 
 
 
-        }
+    }
 
     //Function to reset colors of buttons to white
     fun resetNavColors() {
@@ -115,25 +107,34 @@ class MainActivity : AppCompatActivity() {
         ImageViewCompat.setImageTintList(feedbackBtn, ColorStateList.valueOf(getColor(R.color.white)))
     }
 
-    // Function to change fragment
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.commit()
+
+    // Replace fragment and update nav button color
+    fun replaceFragment(fragment: Fragment) {
+        resetNavColors()
+
+        when (fragment) {
+            is HomeFragment -> ImageViewCompat.setImageTintList(homeBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
+            is AboutFragment -> ImageViewCompat.setImageTintList(aboutBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
+            is SampleDecideFragment -> ImageViewCompat.setImageTintList(recycleBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
+            is FaqFragment -> ImageViewCompat.setImageTintList(faqBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
+            is feedbackFragment -> ImageViewCompat.setImageTintList(feedbackBtn, ColorStateList.valueOf(getColor(R.color.yellow)))
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 
+    // Called when onboarding is finished
     fun finishOnboarding() {
-        // Hide the ViewPager
         val pager = findViewById<ViewPager>(R.id.pager)
         pager.visibility = View.GONE
 
         val dots = findViewById<View>(R.id.dots_indicator)
         dots.visibility = View.GONE
 
-        // Load the main fragment into fragment_container
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, SampleDecideFragment())
-            .commit()
+        // Open SampleDecideFragment → Recycle button turns yellow
+        replaceFragment(SampleDecideFragment())
     }
 
 }
--- app/src/main/java/com/example/sort_it_json/PageAdapter.kt
+++ app/src/main/java/com/example/sort_it_json/PageAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 
 class PageAdapter(
     private val layouts: IntArray,
@@ -14,45 +13,36 @@ class PageAdapter(
 ) : PagerAdapter() {
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        // LayoutInflater allows you to create Views from XML layout files
-        val inflater = LayoutInflater.from(context)
 
-        // Inflate the layout at the current position in the list
-        // layouts[position] refers to the XML layout resource ID
-        // container is the parent viewgroup
-        // false = don’t attach to parent yet (we’ll add it manually later)
+        val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(layouts[position], container, false)
 
-        // If this is the last slide, attach Start button listener
+        // If this is the last slide
         if (position == layouts.size - 1) {
-            // Find the Start button inside this slide
-            val button = view.findViewById<Button>(R.id.startButton)
 
-            // Set a click listener for the Start button
+            val button = view.findViewById<Button>(R.id.startButton)
             button.setOnClickListener {
-                // Call Activity to finish onboarding
-                (context as MainActivity).finishOnboarding()
+                if (context is MainActivity) {
+                    // Open SampleDecideFragment & highlight Recycle
+                    context.finishOnboarding()
+                }
             }
         }
 
-        // Add the newly inflated view to the container (ViewPager)
         container.addView(view)
 
         return view
     }
 
-    // Called when a page is no longer needed by the ViewPager
-    // Removes the page (view) from the container to free up memory
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as View)
+    override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
+        container.removeView(obj as View)
     }
 
-    // Returns the total number of pages/slides in the adapter
-    // In this case, the number of layouts in the onboarding sequence
-    override fun getCount(): Int = layouts.size
+    override fun getCount(): Int {
+        return layouts.size
+    }
 
-    // Determines whether a page View is associated with a specific key object
-    // The ViewPager uses this to check if a page returned from instantiateItem()
-    // is the same as the one currently being displayed
-    override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view == obj
+    }
 }
